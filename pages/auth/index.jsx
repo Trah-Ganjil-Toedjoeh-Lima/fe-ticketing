fa6a35db7b31513051f8f4a4a48e8c782ddaa74c
muhammadrifatba <muhammadrifatba@gmail.com>
2023-04-06 16:10:34 +0700

    feat(Login):init login page

---
+++ pages/auth/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { UserIcon } from "@heroicons/react/24/outline";
+import image from "../../public/herotemp.jpg";
+
+export default function index() {
+  return (
+    <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+      <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
+        {/* leftside */}
+        <div className=" flex flex-col justify-center p-8 md:p-14">
+          <span className="mb-3 text-4xl font-bold"> Welcome to GC GMCO</span>
+          <span className="mb-3 text-4xl font-bold"> Login</span>
+          <form action="">
+            <div className="py-4">
+              <span class="mb-2 text-md">Email</span>
+              <input
+                type="text"
+                class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+                name="email"
+                id="email"
+              />
+            </div>
+            <button class="w-full bg-black text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300">
+              Sign in
+            </button>
+          </form>
+        </div>
+      </div>
+      {/* right side */}
+      <div class="relative">
+        <img
+          src="{image}"
+          alt=""
+          className="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
+        />
+        <div class="absolute hidden bottom-10 right-6 p-6 bg-white bg-opacity-30 backdrop-blur-sm rounded drop-shadow-lg md:block">
+          <span class="text-white text-xl">Welcome to GC GMCO</span>
+        </div>
+      </div>
+    </section>
+  );
+}
