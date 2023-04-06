4b197d0dbb056c758f8690a9a8f2b4009fb9ad49
muhammadrifatba <muhammadrifatba@gmail.com>
2023-04-06 17:14:03 +0700

    feat(tailwind):create custom color

--- pages/auth/index.jsx
+++ pages/auth/index.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-
 export default function index() {
   return (
     <section className="bg-gray-50 min-h-screen flex items-center justify-center">
-      <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
+      <div className="relative flex flex-wrap-reverse m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
         {/* leftside */}
         <div className=" flex flex-col justify-center p-8 md:p-14">
           <span className="mb-3 text-4xl font-bold"> Welcome to GC GMCO</span>
@@ -19,17 +18,17 @@ export default function index() {
                 id="email"
               />
             </div>
-            <button class="w-full bg-black text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300">
+            <button class="w-full bg-gmco-blue-main text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300">
               Sign in
             </button>
           </form>
         </div>
         {/* right side */}
-        <div class="relative">
+        <div class="relative md:mb-0 md:w-3/12 lg:w-4/12">
           <img
             src="/herotemp.jpg"
             alt=""
-            className="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
+            className="w-full h-full hidden rounded-r-2xl md:block object-cover"
           />
           <div class="absolute hidden top-5 right-6 p-6 bg-white bg-opacity-30 backdrop-blur-sm rounded drop-shadow-lg md:block">
             <span class="text-white text-xl">Welcome to GC GMCO</span>
