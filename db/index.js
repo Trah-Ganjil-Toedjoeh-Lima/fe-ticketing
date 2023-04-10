8e827ca982e3a55a544d7e6209752dcf07fb86d8
daffaromero <daffaromero@mail.ugm.ac.id>
2023-04-10 18:04:07 +0700

    feat(admin): Add JSON server for developmental purposes

---
+++ db/index.js
@@ -0,0 +1,20 @@
+// JSON server as placeholder for back-end API
+
+// Terminal command: npx json-server --watch db/index.js --port 3001
+// Resources
+// http://localhost:3001/adminSeatDetails
+// http://localhost:3001/appConfig
+// http://localhost:3001/registerUser
+// http://localhost:3001/verifyOtp
+
+const adminSeatDetails = require("./adminSeatDetails.json");
+const appConfig = require("./appConfig.json");
+const registerUser = require("./registerUser.json");
+const verifyOtp = require("./verifyOtp.json");
+
+module.exports = () => ({
+  adminSeatDetails: adminSeatDetails,
+  appConfig: appConfig,
+  registerUser: registerUser,
+  verifyOtp: verifyOtp,
+});
