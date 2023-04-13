468cdfd37d3d7575d68f9f8e5896bda040004d52
yosefadi <me@yosefadi.my.id>
2023-04-13 17:46:11 +0700

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,3 +4,15 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://dev.gmco-event.com/api/:path*',
+        },
+      ]
+    },
+  }
+  
