b7d2de6f53586d702d2404d9e26b5505302fa5fc
yosefadi <me@yosefadi.my.id>
2023-04-15 16:58:26 +0700

    chore: use baseURL

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -221,3 +221,12 @@ export default function Profile() {
     </div>
   );
 }
+
+export async function getServerSideProps(ctx) {
+  const { req } = ctx;
+  let baseURL = "";
+  if (`https://${req.headers.host}/` === process.env.NEXT_PUBLIC_BASE_URL) {
+    baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+  }
+  return { props: {} };
+}
