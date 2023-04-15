88a5035490d7481b9453ce6d88ddc5bd33b18949
muhammadrifatba <muhammadrifatba@gmail.com>
2023-04-15 19:53:15 +0700

    feat(auth): fixing layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
+    // fontFamily:{
+    //   'sans':[]
+    // }, 
     extend:{
         colors:{
           'gmco-blue':{
