f212ce16fc76cd64c84850bbc1a8a740496d83a2
daffaromero <daffaromero@mail.ugm.ac.id>
2023-04-15 20:04:24 +0700

    feat(admin): Open/close gate DONE

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -5,7 +5,19 @@ import { axiosInstance } from "@/atoms/config";
 
 export default function AdminLogin() {
   const router = useRouter();
+
+  useEffect(() => {
+    // Check if admin is logged in
+    if (typeof window !== "undefined") {
+      // If so, redirect to /admin
+      if (localStorage.getItem("auth_token")) {
+        router.push("/admin");
+      }
+    }
+  }, []);
+
   const [loginInput, setLoginInput] = useState({
+    name: "",
     email: "",
     phone: "",
   });
@@ -21,13 +33,17 @@ export default function AdminLogin() {
       console.log(JSON.stringify(loginInput));
       await axiosInstance
         .post("/api/v1/user/login", {
+          name: loginInput.name,
           email: loginInput.email,
           phone: loginInput.phone,
         })
         .then((res) => {
           if (res.status === 200) {
             console.log(res.data);
-            localStorage.setItem("auth_token", res.data.token.AccessToken);
+            localStorage.setItem(
+              "auth_token",
+              `Bearer ${res.data.token.AccessToken}`
+            );
             router.push("/admin");
           }
         });
@@ -38,7 +54,7 @@ export default function AdminLogin() {
 
   return (
     <div>
-      <div className='py-40 bg-gray-300 backdrop-filter backdrop-blur-sm bg-opacity-50 flex flex-col items-center right-0 p-4 space-y-8 w-full md:w-5/12 md:h-1/2  '>
+      <div className='right-0 flex w-full flex-col items-center space-y-8 bg-gray-300 bg-opacity-50 p-4 py-40 backdrop-blur-sm backdrop-filter md:h-1/2 md:w-5/12  '>
         <div className='flex flex-col items-center '>
           <h1 className='mb-3 text-xl font-bold'>
             {" "}
@@ -52,27 +68,37 @@ export default function AdminLogin() {
           onSubmit={loginSubmit}
         >
           <div className='relative'>
-            <label class='mb-2 text-md'>Email</label>
+            <label class='text-md mb-2'>Name</label>
+            <input
+              type='name'
+              placeholder='Ngademin Siapa?'
+              onChange={(e) => handleInput(e)}
+              value={loginInput.name}
+              class='w-full rounded-md border border-gray-300 p-2 placeholder:font-light placeholder:text-gray-500'
+              id='name'
+            />
+            <label class='text-md mb-2'>Email</label>
             <input
               type='email'
               placeholder='johndoe@mail.com'
               onChange={(e) => handleInput(e)}
               value={loginInput.email}
-              class='w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500'
+              class='w-full rounded-md border border-gray-300 p-2 placeholder:font-light placeholder:text-gray-500'
               id='email'
             />
+            <label class='text-md mb-2'>Phone</label>
             <input
               type='phone'
               placeholder='1-800-273-8255'
               onChange={(e) => handleInput(e)}
               value={loginInput.phone}
-              class='w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500'
+              class='w-full rounded-md border border-gray-300 p-2 placeholder:font-light placeholder:text-gray-500'
               id='phone'
             />
           </div>
           <button
             type='submit'
-            class='w-full bg-gmco-blue text-white p-2 rounded-lg mb-6 hover:bg-gmco-yellow-secondary hover:text-gmco-white hover:border hover:border-gray-300 type'
+            class='type mb-6 w-full rounded-lg bg-gmco-blue p-2 text-white hover:border hover:border-gray-300 hover:bg-gmco-yellow-secondary hover:text-gmco-white'
           >
             Sign in
           </button>
