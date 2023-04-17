15e96d1f74c3dfe345735b61e0db39517555889c
rein7703 <69892731+rein7703@users.noreply.github.com>
2023-04-17 16:03:56 +0700

    Finalize the visual of the profile page

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { Inter, Rubik } from "next/font/google";
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
@@ -17,90 +18,73 @@ export default function Profile() {
   const [email, setEmail] = useState("");
   const [phoneNum, setPhoneNum] = useState("");
   const tickets = [6, 7, 8, 9, 10, 11, 12];
+  const router = useRouter();
+  const [token, setToken] = useState("");
 
-  useEffect(() => {
+  {
+    /*
+useEffect(() => {
     if (typeof window !== "undefined") {
       if (!localStorage.getItem("auth_token")) {
         router.push("/auth");
       }
     }
   }, []);
+*/
+  }
 
-  const HandleInputChange = (event, setStateFunction) => {
+  const handleInputChange = (event, setStateFunction) => {
     setStateFunction(event.target.value);
   };
 
-  const HandleInputKeyDown = (event, setIsEditing) => {
+  const handleInputKeyDown = (event, setIsEditing) => {
     if (event.keyCode === 13) {
       setIsEditing(false);
     }
   };
 
-  const HandleInputBlur = (setIsEditing) => {
+  const handleInputBlur = (setIsEditing) => {
     setIsEditing(false);
   };
 
-  const HandleNameClick = () => {
+  const handleNameClick = () => {
     setIsEditingName(true);
   };
 
-  const HandleEmailClick = () => {
+  const handleEmailClick = () => {
     setIsEditingEmail(true);
   };
 
-  const HandlePhoneNumClick = () => {
+  const handlePhoneNumClick = () => {
     setIsEditingPhoneNum(true);
   };
-  
-
 
   return (
     <>
       {/* HEADER */}
-      <NavigationBar />
-      <div className="bg-gmco-white min-h-screen">
-        {/* PREFVIOUS HEADER JUST IN CASE I BUTUH LAGI
-                <div className="flex flex-row w-full bg-[#932F2F] py-16 items-center">
-          <div className="w-1/2 h-full mx-16 text-start">
-            <h1 className="text-[#F5DB91] font-bold font-rubik text-5xl">
-              PROFILE
-            </h1>
-          </div>
 
-          <div className="w-1/2 mx-16 flex flex-col items-end">
-            <h1 className="text-white font-rubik font-semibold text-xl mt-8">
-              Reinhart Timothy
-            </h1>
-            <p className="text-white font-rubik font-normal">
-              reinhart.siregar@gmail.com
-            </p>
-            <p className="text-white font-rubik font-normal">1-800-273-8255</p>
-          </div>
-        </div>
-        */}
-
-        {/* PROFIL DAN HEADER */}
-
-        <div className="relative">
+      <div className="min-h-screen w-screen bg-gmco-white">
+        <div className="relative w-screen overflow-hidden">
           <img
-            className="h-64 w-full object-cover object-top blur-[3px]"
-            src="GMCO-10.jpg"
+            className="h-64 w-full scale-105 object-cover object-top blur-[5px] brightness-75 "
+            src="GMCO_10.png"
           ></img>
-          <div className="absolute top-0 left-0 w-full h-full flex flex-row py-16 ">
-            <div className="w-1/2 h-full mx-16 text-start">
-              <h1 className="text-[#F5DB91] font-bold font-rubik text-5xl">
-                PROFILE
+          <div className="absolute left-0 top-0 flex h-full w-full flex-col py-16 lg:flex-row">
+            <div className="flex h-full w-1/5 items-center px-16">
+              <h1 className="font-rubik text-5xl font-light text-white">
+                PROFIL
               </h1>
+              <hr class="my-8 h-px border-0 bg-gmco-grey" />
             </div>
 
-            <div className="w-1/2 mx-16 flex flex-col items-end">
-              <h1 className="text-white font-rubik font-semibold text-xl mt-8">
+            <div className="flex w-4/5 flex-col items-start px-16 lg:items-end">
+              <h1 className="mt-8 font-rubik text-xl font-semibold text-[#F5DB91]">
                 Reinhart Timothy
               </h1>
-              <p className="text-white font-rubik font-normal">
+              <p className="font-rubik font-normal text-[#F5DB91]">
                 reinhart.siregar@gmail.com
               </p>
-              <p className="text-white font-rubik font-normal">
+              <p className="font-rubik font-normal text-[#F5DB91]">
                 1-800-273-8255
               </p>
             </div>
@@ -108,14 +92,14 @@ export default function Profile() {
         </div>
 
         {/* CONTENT */}
-        <div className="w-full flex flex-row divide-x">
+        <div className="flex w-full flex-col divide-x lg:flex-row">
           {/* EDIT IDENTITY */}
-          <div className="w-1/3 pt-16 px-8 flex flex-col items-start bg-[#C0925E]">
+          <div className="relative flex w-full flex-col items-start bg-[#C0925E] px-8 py-8 lg:w-1/3">
             {/* Name */}
-            <p className="text-white font-rubik">Nama</p>
+            <p className="font-rubik text-white">Nama</p>
 
             <input
-              className="my-2 w-full text-xl text-start border-transparent focus:ring-gmco-blue focus:border-gmco-blue bg-white rounded-lg"
+              className="mb-8 w-full rounded-lg border-transparent bg-white text-start text-lg focus:border-gmco-blue focus:ring-gmco-blue"
               type="text"
               value={name}
               onChange={(event) => handleInputChange(event, setName)}
@@ -124,11 +108,11 @@ export default function Profile() {
 
             {/*Email*/}
             <div className="flex flex-row">
-              <p className="text-white font-rubik">Email</p>
+              <p className="font-rubik text-white">Email</p>
               <p className="text-red-700">*</p>
             </div>
             <input
-              className="my-2 w-full text-xl text-start border-transparent focus:ring-gmco-blue focus:border-gmco-blue bg-white rounded-lg"
+              className="mb-8 w-full rounded-lg border-transparent bg-white text-start text-lg focus:border-gmco-blue focus:ring-gmco-blue"
               type="text"
               value={email}
               onChange={(event) => handleInputChange(event, setEmail)}
@@ -137,63 +121,69 @@ export default function Profile() {
 
             {/* Phone Number */}
 
-            <p className="text-white font-rubik">Nomor WhatsApp</p>
+            <p className="font-rubik text-white">Nomor WhatsApp</p>
             <input
-              className="my-2 w-full text-xl border-transparent text-start focus:ring-gmco-blue focus:border-gmco-blue bg-white rounded-lg"
+              className="mb-8 w-full rounded-lg border-transparent bg-white text-start text-lg focus:border-gmco-blue focus:ring-gmco-blue"
               type="text"
               value={phoneNum}
               onChange={(event) => handleInputChange(event, setPhoneNum)}
               placeholder="Masukkan Nomor WhatsApp Anda"
             />
-            <button className="w-full rounded-lg bg-[#932F2F] text-center text-white font-inter font-semibold text-xl p-4 mt-16">
+            <button className="mt-12 w-full rounded-lg bg-[#932F2F] p-2 text-center font-inter text-lg font-semibold text-white">
               PERBARUI PROFIL
             </button>
           </div>
 
           {/* List of Tickets */}
-          <div className="w-3/5 flex flex-col gap-4 py-12 pl-8">
-            <p className="text-center text-gmco-grey text-2xl font-medium text-start">
-              My Tickets &#40;{tickets.length}&#41;
+          <div className="flex w-full flex-col gap-4 px-8 py-8 lg:w-2/3">
+            <p className="text-center text-start text-2xl font-medium text-gmco-grey">
+              Pembelian Saya &#40;{tickets.length}&#41;
             </p>
             {/* TICKET */}
 
             {tickets.map((ticket) => (
-              <div className="w-full h-full bg-white flex flex-row rounded-lg p-4">
+              <div className="flex h-full w-full flex-row rounded-lg bg-white p-4">
                 {/* Kursi dan Tipe */}
-                <div className="w-1/5 flex flex-col h-full justify-center text-center">
-                  <h1 className="text-gmco-grey font-rubik font-bold text-4xl inline-block">
+                <div className="flex w-1/5 flex-col justify-center text-center">
+                  <h1 className="font-rubik text-lg font-bold text-gmco-grey sm:text-xl lg:text-2xl">
                     Seat A{ticket}
                   </h1>
-                  <p className="bg-[#F5DB91] text-center text-gmco-grey font-normal inline-block w-full rounded-lg">
+                  <p className="w-full rounded-lg bg-[#F5DB91] text-center text-sm font-normal text-gmco-grey lg:text-base">
                     RADIANT
                   </p>
                 </div>
 
                 {/* Waktu dan Tempat */}
-                <div className="w-full flex flex-row justify-end">
-                  <div className="flex flex-col h-full justify-center text-end text-lg">
+                <div className="flex w-full justify-end items-center">
+                  <div className="flex h-full flex-col justify-center gap-2 text-end text-xs sm:text-sm lg:text-base">
                     <p>Auditorium Driyarkara</p>
                     <p>Sabtu, 27 Mei 2023</p>
                     <p>Open Gate 18.00 WIB</p>
                   </div>
-                  <Image
-                    src="/qris-reinhart.png"
-                    width={100}
-                    height={100}
-                  ></Image>
-                  {/* Nama Konser */}
-                  <div className="w-1/2 flex flex-row bg-gmco-grey rounded-lg items-center p-4">
+                  <div className="overflow-hidden">
                     <Image
-                      src="/violin-picture.png"
-                      width={80}
-                      height={80}
-                      className="2-1/2 justify-start"
+                      src="/qris-reinhart.png"
+                      width={100}
+                      height={100}
                     />
-                    <div className="w-full flex flex-col text-end">
-                      <h1 className="font-inter font-bold text-white font-semibold text-4xl">
+                  </div>
+
+                  {/* Nama Konser */}
+                  <div className="flex w-1/2 items-center rounded-lg bg-gmco-grey py-4 pr-4">
+                    <div className="overflow-hidden mx-2">
+                      <Image
+                        src="/violin-picture.png"
+                        width={80}
+                        height={80}
+                        
+                      />
+                    </div>
+
+                    <div className="flex w-full flex-col text-start sm:text-end">
+                      <h1 className="font-inter text-sm font-bold font-semibold text-white sm:text-lg lg:text-2xl">
                         Grand Concert Vol.10
                       </h1>
-                      <p className="font-inter text-white font-light text-xl">
+                      <p className="font-inter text-sm font-light text-white lg:text-lg">
                         'Anjangsana Simfoni'
                       </p>
                     </div>
@@ -201,8 +191,6 @@ export default function Profile() {
                 </div>
               </div>
             ))}
-
-            {/* This is the ticket */}
           </div>
         </div>
       </div>
