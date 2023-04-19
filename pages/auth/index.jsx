eeac75a151aea505440a83c8437eb73355f1867b
wiweka24 <yogomu24@gmail.com>
2023-04-19 14:06:50 +0700

    patch(): change image tag and query on login

--- pages/auth/index.jsx
+++ pages/auth/index.jsx
@@ -35,18 +35,20 @@ export default function Auth() {
           email: loginInput.email,
         })
         .then((res) => {
-          console.log(res)
-          const otp = res.data.totp_token;
+          console.log(res);
+          // const otp = res.data.totp_token;
           setLoginInput({ ...loginInput, otp: res.data.totp_token });
           if (res.status === 200) {
             // localStorage.setItem("auth_token", res.data.access_token);
-            router.push({
-              pathname: "/auth/otp",
-              query: {
-                email: loginInput.email,
-                otp: res.data.totp_token,
-              },
-            });
+            router.push(
+              // "/auth/otp"
+                {
+                pathname: "/auth/otp",
+                query: {
+                  email: loginInput.email,
+                },
+              }
+            );
           }
         });
     } catch (err) {
@@ -76,40 +78,42 @@ export default function Auth() {
             src="/GMCO.webp"
             alt="bg gmco concert"
             className="object-cover opacity-50"
-            width={2000}
-            height={2000}
+            width={1000}
+            height={1000}
           />
         </div>
-        <div className="relative place ml-3 mt-14  flex h-3/6 w-full flex-col p-4 text-white backdrop-filter md:mt-0 md:w-7/12 md:items-start md:p-10 ">
+        <div className="place relative ml-3 mt-14  flex h-3/6 w-full flex-col p-4 text-white backdrop-filter md:mt-0 md:w-7/12 md:items-start md:p-10 ">
           <h1 className="mb-3 text-4xl font-bold md:text-5xl">
             {" "}
             Grand Concert{" "}
           </h1>
-          <p className='font-base mb-3 text-2xl md:text-3xl'>Vol.10</p>
-          <p className=' font-base text-2xl md:text-2xl'>
+          <p className="font-base mb-3 text-2xl md:text-3xl">Vol.10</p>
+          <p className=" font-base text-2xl md:text-2xl">
             {" "}
             Anjangsana Simfoni{" "}
           </p>
         </div>
-        <img
+        <Image
           src="/logo_gmco.webp"
           alt="logo"
           className="absolute left-5 top-3 w-32 md:left-9 md:top-3/4 md:w-52"
+          width={1000}
+          height={1000}
         />
 
-        <div className='right-0 mt-7 flex w-full flex-col items-center space-y-8 bg-gray-400 bg-opacity-50 p-4 py-32 backdrop-blur-sm backdrop-filter md:mt-0 md:w-5/12 md:py-40 '>
-          <div className='-mt-7 flex flex-col items-center'>
-            <h1 className='mb-3 text-4xl font-bold text-gmco-white'>
+        <div className="right-0 mt-7 flex w-full flex-col items-center space-y-8 bg-gray-400 bg-opacity-50 p-4 py-32 backdrop-blur-sm backdrop-filter md:mt-0 md:w-5/12 md:py-40 ">
+          <div className="-mt-7 flex flex-col items-center">
+            <h1 className="mb-3 text-4xl font-bold text-gmco-white">
               Selamat Datang
             </h1>
           </div>
           <form
-            action='#'
-            className='flex flex-col items-center space-y-4'
+            action="#"
+            className="flex flex-col items-center space-y-4"
             onSubmit={LoginSubmit}
           >
-            <div className='w-full'>
-              <label className=' pl-2 text-base text-gmco-white'>
+            <div className="w-full">
+              <label className=" pl-2 text-base text-gmco-white">
                 Masukkan email anda
               </label>
               <div className="relative flex w-full items-stretch">
@@ -133,12 +137,12 @@ export default function Auth() {
             </label>
 
             <button
-              type='submit'
-              className='mb-6 w-full rounded-full border-2 border-white bg-gmco-orange-secondarylight p-2 font-semibold text-white  hover:bg-gmco-yellow-secondary hover:text-gmco-white'
+              type="submit"
+              className="mb-6 w-full rounded-full border-2 border-white bg-gmco-orange-secondarylight p-2 font-semibold text-white  hover:bg-gmco-yellow-secondary hover:text-gmco-white"
             >
               LOG IN / REGISTER
             </button>
-            <label className='pt-3 text-center text-xs text-gmco-white'>
+            <label className="pt-3 text-center text-xs text-gmco-white">
               Gadjah Mada Chamber Orchestra
             </label>
           </form>
