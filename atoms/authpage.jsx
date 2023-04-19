c5509d02b56ac68adc0f47e9366751c6103e797b
muhammadrifatba <muhammadrifatba@gmail.com>
2023-04-19 21:06:07 +0700

    feat(auth): abort delete auth token when close window/browser

--- atoms/authpage.jsx
+++ atoms/authpage.jsx
@@ -1,16 +1,16 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-export function useClearAuthTokenOnUnload(auth_token) {
-  useEffect(() => {
-    // Store the authentication token in localStorage // Listen for the beforeunload event and remove the authentication token from localStorage
-   const onUnload = () => {
-     localStorage.removeItem("auth_token");
-   };
-   window.addEventListener("unload", onUnload);
+// export function useClearAuthTokenOnUnload(auth_token) {
+//   // useEffect(() => {
+//   //   // Store the authentication token in localStorage // Listen for the beforeunload event and remove the authentication token from localStorage
+//   //   const onUnload = () => {
+//   //     localStorage.removeItem("auth_token");
+//   //   };
+//   //   window.addEventListener("unload", onUnload);
 
-   // Clean up the event listener on unmount
-   return () => {
-     window.removeEventListener("unload", onUnload);
-   };
-  }, [auth_token]);
-}
+//   //   // Clean up the event listener on unmount
+//   //   return () => {
+//   //     window.removeEventListener("unload", onUnload);
+//   //   };
+//   // }, [auth_token]);
+// }
