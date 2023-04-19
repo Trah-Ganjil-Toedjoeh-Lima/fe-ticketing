5c63ba940f9f4311d806b9d6e6457d0283015ac3
muhammadrifatba <muhammadrifatba@gmail.com>
2023-04-19 20:33:11 +0700

    feat(auth): delete auth token when close window/browser

--- atoms/authpage.jsx
+++ atoms/authpage.jsx
@@ -3,14 +3,14 @@ import { useEffect } from "react";
 export function useClearAuthTokenOnUnload(auth_token) {
   useEffect(() => {
     // Store the authentication token in localStorage // Listen for the beforeunload event and remove the authentication token from localStorage
-    const onBeforeUnload = () => {
-      localStorage.removeItem("auth_token");
-    };
-    window.addEventListener("beforeunload", onBeforeUnload);
+   const onUnload = () => {
+     localStorage.removeItem("auth_token");
+   };
+   window.addEventListener("unload", onUnload);
 
-    // Clean up the event listener on unmount
-    return () => {
-      window.removeEventListener("beforeunload", onBeforeUnload);
-    };
+   // Clean up the event listener on unmount
+   return () => {
+     window.removeEventListener("unload", onUnload);
+   };
   }, [auth_token]);
 }
