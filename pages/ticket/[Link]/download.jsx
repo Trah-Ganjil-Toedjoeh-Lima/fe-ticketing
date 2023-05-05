1070be800c9fcbe482fb7d973de34d20800f6a14
yosefadi <me@yosefadi.my.id>
2023-05-06 02:40:58 +0700

    patch(ticket): refactor download ticket code

--- pages/ticket/[Link]/download.jsx
+++ pages/ticket/[Link]/download.jsx
@@ -1,10 +1,24 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { axiosInstance } from "@/utils/config";
+import { saveAs } from 'file-saver';
+import { notifyInfo } from "@/components/notify";
 
 export default function DownloadTicket() {
   const router = useRouter();
   const { Link } = router.query;
+
+  const handleDownload = async (seat_name, user_name) => {
+    const response = await fetch(
+      "https://files.gmco-event.com/tickets/" +
+        seat_name +
+        "_" +
+        Link +
+        ".png"
+    );
+    const blob = await response.blob();
+    saveAs(blob, seat_name + "_" + user_name + ".png");
+  };  
 
   useEffect(() => {
     const fetchTicketData = async () => {
@@ -12,16 +26,10 @@ export default function DownloadTicket() {
         await axiosInstance
           .get(`/api/v1/seat/${Link}`)
           .then((res) => {
-            router.push(
-              "https://files.gmco-event.com/tickets/" +
-                res.data.data.seat_name +
-                "_" +
-                Link +
-                ".png"
-            );
-            return;
+            handleDownload(res.data.data.seat_name, res.data.data.user_name);
+            notifyInfo("Downloading ticket...");
+            router.push("/profile");
           });
-        // console.log(res.data.data);
       } catch (err) {
         router.push("/profile")
       }
