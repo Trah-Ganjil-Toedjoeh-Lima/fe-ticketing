0297042d36f8e0503e03aa3a3173aa644b8b4db0
wiweka24 <yogomu24@gmail.com>
2023-05-05 22:05:32 +0700

    ref(cart): fix midtrans sandbox

--- utils/config.jsx
+++ utils/config.jsx
@@ -29,22 +29,3 @@ axiosInstance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-
-export function midtransSetup(myMidtransClientKey) {
-  // You can also change below url value to any script url you wish to load,
-  // for example this is snap.js for Sandbox Env (Note: remove `.sandbox` from url if you want to use production version)
-  const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
-
-  let scriptTag = document.createElement("script");
-  scriptTag.src = midtransScriptUrl;
-  // Optional: set script attribute, for example snap.js have data-client-key attribute
-  // (change the value according to your client-key)
-  // const midtransClientKey = process.env.NEXT_MIDTRANS_CLIENT_KEY
-  scriptTag.setAttribute("data-client-key", myMidtransClientKey);
-
-  document.body.appendChild(scriptTag);
-
-  return () => {
-    document.body.removeChild(scriptTag);
-  };
-}
