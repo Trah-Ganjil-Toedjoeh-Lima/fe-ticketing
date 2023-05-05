49fa5cbd77f6e92d9d6ae0e5177198e28c2eca24
daffaromero <daffaromero@mail.ugm.ac.id>
2023-05-05 18:36:53 +0700

    feat(TnC): Terms and Conditions page + small changes

--- components/footer.jsx
+++ components/footer.jsx
@@ -34,59 +34,58 @@ export default function FooterBar() {
     },
     {
       title: "Legal",
-      submenu: [
-        { name: "Terms & Conditions", route: "/terms-and-conditions" },
-      ],
+      submenu: [{ name: "Terms & Conditions", route: "/terms-and-conditions" }],
     },
   ];
 
   // Social Media Icon
   const socials = [
-    { name: <FaTiktok className="h-5 w-5" />, route: "/" },
-    { name: <FaWhatsapp className="h-5 w-5" />, route: "/" },
+    { name: <FaTiktok className='h-5 w-5' />, route: "/" },
+    { name: <FaWhatsapp className='h-5 w-5' />, route: "/" },
     {
-      name: <FaInstagram className="h-5 w-5" />,
+      name: <FaInstagram className='h-5 w-5' />,
       route: "https://www.instagram.com/gmco_ugm/",
     },
     {
-      name: <FaTwitter className="h-5 w-5" />,
+      name: <FaTwitter className='h-5 w-5' />,
       route: "https://twitter.com/GMCO_UGM?s=08",
     },
     {
-      name: <FaFacebook className="h-5 w-5" />,
-      route: "https://www.facebook.com/people/Gadjah-Mada-Chamber-Orchestra/100067509763569/",
+      name: <FaFacebook className='h-5 w-5' />,
+      route:
+        "https://www.facebook.com/people/Gadjah-Mada-Chamber-Orchestra/100067509763569/",
     },
     {
-      name: <FaYoutube className="h-5 w-5" />,
+      name: <FaYoutube className='h-5 w-5' />,
       route: "https://www.youtube.com/channel/UC0VSyJsls-u84buKVgWsBUA",
     },
   ];
 
   return (
-    <footer className="bg-gmco-grey px-8 pb-6 pt-10 text-gmco-white md:px-8 lg:px-48">
-      <div className="mb-4 grid grid-cols-2 gap-8 md:mb-16 md:grid-cols-5 md:gap-2 lg:grid-cols-7">
-        <div className="col-span-2 md:mr-24 lg:col-span-4">
-          <div className="flex items-center">
+    <footer className='bg-gmco-grey px-8 pb-6 pt-10 text-gmco-white md:px-8 lg:px-48'>
+      <div className='mb-4 grid grid-cols-2 gap-8 md:mb-16 md:grid-cols-5 md:gap-2 lg:grid-cols-7'>
+        <div className='col-span-2 md:mr-24 lg:col-span-4'>
+          <div className='flex items-center'>
             <Image
-              src="/logo-anjangsana.webp"
-              alt="logo gmco"
-              className="mr-3 h-12 md:h-20 w-max"
+              src='/logo-anjangsana.webp'
+              alt='logo gmco'
+              className='mr-3 h-12 w-max md:h-20'
               width={1000}
               height={1000}
             />
-            <span href="#" className="text-xl font-bold">
+            <span href='#' className='text-xl font-bold'>
               Grand Concert #10 <br />{" "}
-              <span className="text-base font-medium">Anjangsana Simfoni</span>
+              <span className='text-base font-medium'>Anjangsana Simfoni</span>
             </span>
           </div>
         </div>
         {menus.map((menu, index) => (
           <div key={index}>
-            <div className="mb-4 text-xl font-bold">{menu.title}</div>
-            <ul className="space-y-2 text-gmco-white/60">
+            <div className='mb-4 text-xl font-bold'>{menu.title}</div>
+            <ul className='space-y-2 text-gmco-white/60'>
               {menu.submenu.map((submenu, index) => (
                 <li key={index}>
-                  <Link href={submenu.route} className="hover:text-gmco-white">
+                  <Link href={submenu.route} className='hover:text-gmco-white'>
                     {submenu.name}
                   </Link>
                 </li>
@@ -97,17 +96,17 @@ export default function FooterBar() {
       </div>
 
       {/* Divider */}
-      <div className="my-6 border-t border-gray-300 "></div>
+      <div className='my-6 border-t border-gray-300 '></div>
 
       {/* Bawah */}
-      <div className="inline items-center justify-between md:flex">
-        <div className="text-sm text-gray-500">
+      <div className='inline items-center justify-between md:flex'>
+        <div className='text-sm text-gray-500'>
           &copy;
-          <Link href="" className="font-bold hover:text-gmco-white">
+          <Link href='' className='font-bold hover:text-gmco-white'>
             GMCO UGM{" "}
           </Link>
-          courtesy by
-          <Link href="" className="font-bold hover:text-gmco-white">
+          courtesy of
+          <Link href='' className='font-bold hover:text-gmco-white'>
             {" "}
             Trah Ganjil 75{" "}
           </Link>
@@ -115,12 +114,12 @@ export default function FooterBar() {
         </div>
 
         {/* to do : cari icon */}
-        <div className="mt-2 flex space-x-4 md:m-0">
+        <div className='mt-2 flex space-x-4 md:m-0'>
           {socials.map((social, index) => (
             <Link
               key={index}
               href={social.route}
-              className="text-gmco-white/50 hover:text-gmco-white"
+              className='text-gmco-white/50 hover:text-gmco-white'
             >
               {social.name}
             </Link>
