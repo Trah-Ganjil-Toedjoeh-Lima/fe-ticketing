b48fde709c1ce10dbd215696e0bf413f56f43a97
daffaromero <daffaromero@mail.ugm.ac.id>
2023-05-06 13:57:23 +0700

    Fixed build errors

--- pages/ticket/[Link]/download.jsx
+++ pages/ticket/[Link]/download.jsx
@@ -4,38 +4,36 @@
 // import { saveAs } from 'file-saver';
 // import { notifyInfo } from "@/components/notify";
 
-// export default function DownloadTicket() {
-//   const router = useRouter();
-//   const { Link } = router.query;
-
-//   const handleDownload = async (seat_name, user_name) => {
-//     const response = await fetch(
-//       "https://files.gmco-event.com/tickets/" +
-//         seat_name +
-//         "_" +
-//         Link +
-//         ".png"
-//     );
-//     const blob = await response.blob();
-//     saveAs(blob, seat_name + "_" + user_name + ".png");
-//   };  
-
-//   useEffect(() => {
-//     const fetchTicketData = async () => {
-//       try {
-//         await axiosInstance
-//           .get(`/api/v1/seat/${Link}`)
-//           .then((res) => {
-//             handleDownload(res.data.data.seat_name, res.data.data.user_name);
-//             notifyInfo("Downloading ticket...");
-//             router.push("/profile");
-//           });
-//       } catch (err) {
-//         router.push("/profile")
-//       }
-//     }; // end of fetchTicketData
-//     fetchTicketData();
-//     router.push("/profile")
-//   }, []);
-//   return null;
-// }
+export default function DownloadTicket() {
+  //   const router = useRouter();
+  //   const { Link } = router.query;
+  //   const handleDownload = async (seat_name, user_name) => {
+  //     const response = await fetch(
+  //       "https://files.gmco-event.com/tickets/" +
+  //         seat_name +
+  //         "_" +
+  //         Link +
+  //         ".png"
+  //     );
+  //     const blob = await response.blob();
+  //     saveAs(blob, seat_name + "_" + user_name + ".png");
+  //   };
+  //   useEffect(() => {
+  //     const fetchTicketData = async () => {
+  //       try {
+  //         await axiosInstance
+  //           .get(`/api/v1/seat/${Link}`)
+  //           .then((res) => {
+  //             handleDownload(res.data.data.seat_name, res.data.data.user_name);
+  //             notifyInfo("Downloading ticket...");
+  //             router.push("/profile");
+  //           });
+  //       } catch (err) {
+  //         router.push("/profile")
+  //       }
+  //     }; // end of fetchTicketData
+  //     fetchTicketData();
+  //     router.push("/profile")
+  //   }, []);
+  //   return null;
+}
